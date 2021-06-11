9a253234b30d0859a8d0f8ad833fc72ee82cd5ff
Josip Matoković <jmatokovi@foi.hr>
2021-06-11 11:08:15 +0200

    test-jmatokovi

--- Software/reRack/reRack.Design/Program.cs
+++ Software/reRack/reRack.Design/Program.cs
@@ -8,7 +8,7 @@ namespace reRack.Design
 {
     static class Program
     {
-        //final
+        //metoksid
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
