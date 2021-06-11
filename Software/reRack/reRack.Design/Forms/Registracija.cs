13dfebbdc0e02c6a6dd238972f11e528802dd3f0
Kristijan Čulina <kculina@foi.hr>
2021-06-11 13:47:20 +0200

    Dodana baza

--- Software/reRack/reRack.Design/Forms/Registracija.cs
+++ Software/reRack/reRack.Design/Forms/Registracija.cs
@@ -12,14 +12,16 @@ namespace reRack.Design.Forms
 {
     public partial class Registracija : Form
     {
+        Entities entities = new Entities();
         public Registracija()
         {
             InitializeComponent();
         }
 
-        private void uxActionRegistrirajSe_Click(object sender, EventArgs e)
+        private void Registracija_Load(object sender, EventArgs e)
         {
-
+            List<Grad> grads = entities.Grad.ToList();
+            uiGrad.DataSource = grads;
         }
     }
 }
